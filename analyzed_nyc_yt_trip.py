5064129fc72ce6e1c29a9a89b11428d855df4e5f
Vincenzo Maria Calandra <vincenzomaria.calandra@gmail.com>
2022-12-16 19:53:37 +0100

    Minor changes to readByYearMonthBorough

--- analyzed_nyc_yt_trip.py
+++ analyzed_nyc_yt_trip.py
@@ -3,8 +3,8 @@ from utilities.read_data import readByYearMonthBorough
 
 if __name__ == "__main__":
     # Read the dataset
-    readByYearMonthBorough(2020,1,"Bronx")
-
+    df = readByYearMonthBorough(2022,1,"Bronx")
+    
     # Clean the dataset
 
     # Analyzed the dataset
--- utilities/read_data.py
+++ utilities/read_data.py
@@ -1,8 +1,9 @@
 import pandas as pd
 
-URL = "https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_"
+YT_TRIP_DATASET_URL = "https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_"
+TAXI_ZONE_LOOKUP_DATASET_URL="https://d37ci6vzurychx.cloudfront.net/misc/taxi+_zone_lookup.csv"
 
-def readByYearMonthBorough(year: int,month: int,borough: str):
+def readByYearMonthBorough(year: int,month: int,borough: str) -> pd.DataFrame:
     '''This func download the specified yellow taxi data set from TLC website.
 
     Parameters:
@@ -14,17 +15,38 @@ def readByYearMonthBorough(year: int,month: int,borough: str):
     pandas.core.frame.DataFrame :Return the pandas df of @year, @month, @borough selected.
     '''
 
+    zone_lookup_df=pd.read_csv(TAXI_ZONE_LOOKUP_DATASET_URL)
+
     #Check if month is tidy
+    if (month<1 or month >13):
+        raise ValueError("Month provided can't be less than 1 or greater than 12")
     #Check if year is tidy
+    if (year<2009 or year>2022):
+        raise ValueError("Year provided can't be less than 2009 or greater than 2022")
     #Check if borough is tidy
+    if (borough not in zone_lookup_df["Borough"].unique()):
+        raise ValueError("Borough provided {} is not valid, should be one of these: {}".format(borough,zone_lookup_df["Borough"].unique()))
 
     #Format the url
-    DATA_URL = URL  + str(year) + "-" + str(month).zfill(2) + ".parquet"
+    DATA_URL = YT_TRIP_DATASET_URL  + str(year) + "-" + str(month).zfill(2) + ".parquet"
 
     #Download data of @month @year specified
     df = pd.read_parquet(DATA_URL)
-
-    #Trasform the download
-    df.drop(df[(df['PULocationID'] != borough)].index,inplace=True)
     
+    #Merged the two dataframes on PULocationID and DOLocationID
+    df = pd.merge(df,zone_lookup_df[["LocationID","Borough"]],left_on="PULocationID",right_on="LocationID")
+    df.rename(columns={"Borough":"PULocation"}, inplace=True)
+    df.drop('PULocationID', axis=1, inplace=True)
+    df.drop('LocationID', axis=1, inplace=True)
+
+    df = pd.merge(df,zone_lookup_df[["LocationID","Borough"]],left_on="DOLocationID",right_on="LocationID")
+    df.rename(columns={"Borough":"DOLocation"}, inplace=True)
+    df.drop('DOLocationID', axis=1, inplace=True)
+    df.drop('LocationID', axis=1, inplace=True)
+
+    #Drop records where PULocation is different than specified @borough
+    df.drop(df[(df['PULocation'] != borough)].index,inplace=True)
+
+    #Reindex the returned dataframe
+    df = df.reset_index(drop=True)
     return df
