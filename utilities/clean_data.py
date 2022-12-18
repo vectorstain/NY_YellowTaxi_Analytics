50385c6a063da5a7ce7ba068c2ae2daac6751f5b
Vincenzo Maria Calandra <vincenzomaria.calandra@gmail.com>
2022-12-18 17:39:51 +0100

    Updated clean func

--- utilities/clean_data.py
+++ utilities/clean_data.py
@@ -1,82 +1,97 @@
 import pandas as pd
 import random as r
 
+TAXI_ZONE_LOOKUP_DATASET_URL="https://d37ci6vzurychx.cloudfront.net/misc/taxi+_zone_lookup.csv"
+
+
 def removeMessyRows(df: pd.DataFrame) -> pd.DataFrame:
+    
+    print(f"Starting with {len(df.index)} rows.")
     
     #Remove duplicates
     df.drop_duplicates(inplace=True)
+    print(f"Removed duplicated rows. Now df has: {len(df.index)} ")
 
     #Now let's remove from from the table the rows with the passenger_count == 0 or NaN (null)
     df.drop(df[(df['passenger_count'] == 0)|(df["passenger_count"].isna())].index,inplace=True)
+    print(f"Removed rows where passenger_count eq. 0 or NaN. Now df has: {len(df.index)} ")
 
     #Now let's remove from from the table the rows with the fare_amount == 0 or NaN (null)
     df.drop(df.loc[df.fare_amount<=0].index, inplace=True)
+    print(f"Removed rows where fare_amount eq. 0 or NaN. Now df has: {len(df.index)} ")
 
-    #Now let's remove from from the table the rows with the congestion_surcharge == 0 or NaN (null)
-    df.drop(df.loc[df.congestion_surcharge<=0].index, inplace=True)
+    #Now let's remove from from the table the rows with the congestion_surcharge is less than 0.
+    df.drop(df.loc[df.congestion_surcharge<0].index, inplace=True)
+    print(f"Removed rows where congestion_surcharge is less than 0. Now df has: {len(df.index)} ")
 
-    #null_trip are rows where trip_distance == 0 and trip duration is under 10minuts
-    null_trip = df.loc[(df['tpep_dropoff_datetime']-df['tpep_pickup_datetime']<600) & (df['trip_distance'] == 0)]
-    #Now let's remove from from the table the rows with the passenger_count == 0 or NaN (null)
+    #null_trip are rows where trip_distance == 0 and trip duration is under 5minuts
+    null_trip = df.loc[(df['tpep_dropoff_datetime']-df['tpep_pickup_datetime']<300) & (df['trip_distance'] == 0)]
     df.drop(null_trip.index,inplace=True)
+    print(f"Removed rows where trip_distance eq. to 0 and trip time under 5 minuts. Now df has: {len(df.index)} ")
 
     #Let's see districts with unique PULocation values
-    districs = df["PULocationZone"].unique()
-    
+    PULdistrics = df["PULocationZone"].unique()
+    DOLdistrics = df["DOLocationZone"].unique()
+
     #Let's create a dictionary with the values ​​of PULocation, DOLocation and Average trip
     dict_avg_trip = {
         "PUL" : [],
         "DOL" : [],
-        "AVG_TRIP": []
+        "AVG_TRIP": [],
+        "AVG_DUR_S": []
     }
 
     #Create a table with the information of PULocationID, DOLocationID and Average trip
-    for i in range(len(districs)):
-        for j in range(len(districs)):
-            PUL=districs[i]
-            DOL=districs[j]
-            avg_trip = df.query("PULocationZone == @PUL and DOLocationZone == @DOL")["trip_distance"].mean()
+    for i in range(len(PULdistrics)):
+        for j in range(len(DOLdistrics)):
+            PUL=PULdistrics[i]
+            DOL=DOLdistrics[j]
+            avg_trip_query = df.query("PULocationZone == @PUL and DOLocationZone == @DOL")
+            avg_trip = avg_trip_query["trip_distance"].mean()
             dict_avg_trip["PUL"].append(PUL)
             dict_avg_trip["DOL"].append(DOL)
             dict_avg_trip["AVG_TRIP"].append(avg_trip)
 
+            duration=0
+            for k in avg_trip_query.index:
+                duration = duration + (avg_trip_query["tpep_dropoff_datetime"][k]-avg_trip_query["tpep_pickup_datetime"][k])
+
+            #print(PUL+"->"+DOL+": "+str(avg_duration))    
+            avg_duration = duration/avg_trip_query.__len__() if avg_trip_query.__len__() != 0 else 0
+            dict_avg_trip["AVG_DUR_S"].append(avg_duration)
+
+    #Merged the two dataframes on PULocationID and DOLocationID
     avg_trip_df = pd.DataFrame(dict_avg_trip)
+    zone_lookup_df=pd.read_csv(TAXI_ZONE_LOOKUP_DATASET_URL)
+    avg_trip_df = pd.merge(avg_trip_df,zone_lookup_df[["Borough","Zone"]],left_on="PUL",right_on="Zone")
+    avg_trip_df.rename(columns={"Borough":"PULBorough"}, inplace=True)
+    avg_trip_df = pd.merge(avg_trip_df,zone_lookup_df[["Borough","Zone"]],left_on="DOL",right_on="Zone")
+    avg_trip_df.rename(columns={"Borough":"DOLBorough"}, inplace=True)
+    avg_trip_df.drop_duplicates(inplace=True)
 
     #Lets find others accorency where trip_distance is equal to 0s
     null_trip = df.loc[(df['trip_distance'] == 0)]
-    null_trip.reset_index()
 
     #Let's take the null_trips considered above and replace the values ​​that have trip_distance == 0 or NaN with the AVG_TRIP of the route
-    for i in range(len(null_trip.index)):
-        PULL=null_trip.iloc[i]["PULocationZone"]
-        DOL=null_trip.iloc[i]["DOLocationZone"]
-        INDEX=null_trip.index[i]
+    for i in null_trip.index.to_list():
+        PULL=null_trip["PULocationZone"][i]
+        DOL=null_trip["DOLocationZone"][i]
         AVG=avg_trip_df.loc[(avg_trip_df["PUL"]==PULL)&(avg_trip_df["DOL"]==DOL)]["AVG_TRIP"]
-        df.at[INDEX,"trip_distance"] = AVG
+        #print(f"AVG{AVG.to_list()},PUL:{PUL},DOL:{DOL}")
 
-    #Lets check unkown trips
-    unknow_trip = df.loc[(df['DOLocation'] == 'Unknown')|(df['PULocation'] == 'Unknown')]
-    avg_trip_df["AVG_DUR_S"] = 0
+        if (AVG.any()):
+            df.at[i,"trip_distance"] = AVG
+        else:
+            df.drop(i,inplace=True)
+    print(f"Removed rows where  trip_distance == 0 and doesnt exist an alternative trip. Now df has: {len(df.index)} ")
 
-    # Lets compute avg trip duration for each PUL DOL combo
-    for i in avg_trip_df.index:
-        PUL=avg_trip_df["PUL"][i]
-        DOL=avg_trip_df["DOL"][i]
-        PUL_DOL_trips_df = df.query("PULocationZone == @PUL and DOLocationZone == @DOL")
-        duration=0
-        for j in PUL_DOL_trips_df.index:
-            duration = duration + (PUL_DOL_trips_df["tpep_dropoff_datetime"][j]-PUL_DOL_trips_df["tpep_pickup_datetime"][j])
-
-        #print(PUL+"->"+DOL+": "+str(avg_duration))    
-        avg_duration = duration/PUL_DOL_trips_df.__len__() if PUL_DOL_trips_df.__len__() != 0 else 0
-        avg_trip_df.at[i,"AVG_DUR_S"] = avg_duration
 
     avg_trip_df.drop(avg_trip_df.loc[(avg_trip_df["PUL"]=="NV")|(avg_trip_df["DOL"]=="NV")].index, inplace=True)
     avg_trip_df.drop(avg_trip_df.loc[(avg_trip_df["PUL"].isna())|(avg_trip_df["DOL"].isna())].index, inplace=True)
+    
+    #Lets check unkown trips
+    unknow_trip = df.loc[(df['DOLocation'] == 'Unknown')|(df['PULocation'] == 'Unknown')|(df['PULocationZone'] == 'NV')|(df['PULocationZone'].isna())|(df['DOLocationZone'] == 'NV')|(df['DOLocationZone'].isna())]
 
-
-        
-    dropped=0
     for i in unknow_trip.index:
         PUL = unknow_trip["PULocationZone"][i]
         DOL = unknow_trip["DOLocationZone"][i]
@@ -97,8 +112,8 @@ def removeMessyRows(df: pd.DataFrame) -> pd.DataFrame:
             df.at[i,"PULocationZone"] = similar_trips.at[rand_row_index,"PUL"]
             df.at[i,"DOLocationZone"] = similar_trips.at[rand_row_index,"DOL"]
         else:
-            dropped=dropped+1
             df.drop(i,inplace=True)
+    print(f"Removed rows where PUL/DOL zone is eq to NV or NaN and PUL/DOL Borough is Unknows. Now df has: {len(df.index)} ")
 
     return df
 
