01001fa3420a36ec904944d5f2670a3c4fb41791
Vincenzo Maria Calandra <vincenzomaria.calandra@gmail.com>
2022-12-21 18:52:38 +0100

    Added analyses func + minor changes

--- analyzed_nyc_yt_trip.py
+++ analyzed_nyc_yt_trip.py
@@ -1,7 +1,7 @@
 # Local Utilities
 from utilities.read_data import readBoroughTripsByYearMonth
 from utilities.clean_data import getCleanedDataFrame
-
+from utilities.analyze_data import computeAverageFareAmountPerMile, computeAverageFareAmountPerMileInTime
 if __name__ == "__main__":
 
     # read user input
@@ -14,7 +14,8 @@ if __name__ == "__main__":
 
     # Clean the dataset
     df = getCleanedDataFrame(df)
-    
-    # Analyzed the dataset
 
+    # Analyzed the dataset
+    print(f"The average PM is:{computeAverageFareAmountPerMile(df)}")
+    print(f"The average PMT is:{computeAverageFareAmountPerMileInTime(df)}")
     # Print/Save report/analysis
---
+++ utilities/analyze_data.py
@@ -0,0 +1,41 @@
+import pandas as pd
+
+def computeAverageFareAmountPerMile(df: pd.DataFrame) -> float:
+    '''This func download the specified yellow taxi data set from TLC website.
+
+    Parameters:
+    year (int): The year you would analyzed, e.g. 2020 2021 2022.
+    month (int): The n. th. month you would analyzed, e.g. 1 for Jen, 2 for Feb, 3 for May.
+    borough (str): The borough you would analyzed, e.g. Manhattan, Bronx.
+
+    Returns:
+    pandas.core.frame.DataFrame :Return the pandas df of @year, @month, @borough selected.
+    '''
+
+    #Lets compute
+    df["PM"]=pd.Series({})
+    df["PM"]=df.apply(lambda x: x["fare_amount"]/x["trip_distance"], axis=1)
+
+    AVG = df["PM"].mean()
+
+    return AVG
+
+def computeAverageFareAmountPerMileInTime(df: pd.DataFrame) -> float:
+    '''This func download the specified yellow taxi data set from TLC website.
+
+    Parameters:
+    year (int): The year you would analyzed, e.g. 2020 2021 2022.
+    month (int): The n. th. month you would analyzed, e.g. 1 for Jen, 2 for Feb, 3 for May.
+    borough (str): The borough you would analyzed, e.g. Manhattan, Bronx.
+
+    Returns:
+    pandas.core.frame.DataFrame :Return the pandas df of @year, @month, @borough selected.
+    '''
+
+    #Lets compute
+    df["PMT"]=pd.Series({})
+    df["PMT"]=df.apply(lambda x: (x["fare_amount"]/x["trip_distance"])/(x["tpep_dropoff_datetime"]-x["tpep_pickup_datetime"]), axis=1)
+
+    AVG = df["PMT"].mean()
+
+    return AVG
