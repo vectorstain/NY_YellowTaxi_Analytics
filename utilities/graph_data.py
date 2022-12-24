f438334b9aee2fd41ae2760201ca936be231a206
Vincenzo Maria Calandra <vincenzomaria.calandra@gmail.com>
2022-12-24 16:56:45 +0100

    Added graph_data.py

---
+++ utilities/graph_data.py
@@ -0,0 +1,15 @@
+import pandas
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+def graphBoxplot(df: pandas.DataFrame) -> None:
+    pass
+
+def graphBarchart(df: pandas.DataFrame) -> None:
+    pass
+
+def graphHeatmap(df: pandas.DataFrame) -> None:
+    pass
+
+def graphScatterplot(df: pandas.DataFrame) -> None:
+    pass
